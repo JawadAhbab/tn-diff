58d1fa52666f0d5343983ce5b9dc1801475849dc
JawadAhbab <jawad.ahbab.turna@gmail.com>
2023-10-27 02:37:27 +0600

    🔥 Misc

--- src/core/diffstr.ts
+++ src/core/diffstr.ts
@@ -2,7 +2,6 @@ import { diffWords } from 'diff'
 type DiffStrChange = [1 | -1, string]
 export type DiffStr = (DiffStrChange | number)[]
 
-// ARGENT reset git
 export const diffstr = (prev: string, next: string) => {
   return diffWords(prev, next).map<DiffStr[number]>((d) => {
     if (d.added) return [1, d.value]
